d9dd73a9d5789dcb5b0b2dcdd5c49c3676fa63cf
koder <koder.mail@gmail.com>
2019-02-13 12:02:27 +0000

    improve deploy.sh - fix coding style

--- deploy.sh
+++ deploy.sh
@@ -1,209 +1,304 @@
 #!/usr/bin/env bash
-set -e -u -o pipefail -o errexit
+set -o nounset
+set -o pipefail
+set -o errexit
 
 # CONFIGURABLE
 
 #COMPRESSOR="gzip -f -k"
 #EXT="gz"
 
-COMPRESSOR="lzma -9 -z -k -f"
-EXT="lzma"
+readonly COMPRESSOR="lzma -9 -z -k -f"
+readonly EXT="lzma"
 
-RECORD_DURATION=60
-RECORD_SIZE=100
+readonly RECORD_DURATION=60
+readonly RECORD_SIZE=100
 
-ALL_NODES="ceph01 ceph02 ceph03"
-#ALL_NODES="ceph01 ceph02 ceph03 ceph04 ceph05 ceph06 ceph07 ceph08 ceph09 ceph10"
-#ALL_NODES="$ALL_NODES ceph11 ceph12 ceph13 ceph14 ceph15 ceph16 ceph17 ceph18 ceph19"
-#ALL_NODES="$ALL_NODES ceph20 ceph21 ceph22 ceph23 ceph24 ceph25 ceph26 ceph27 ceph28 ceph29"
-#ALL_NODES="$ALL_NODES ceph30 ceph31 ceph32 ceph33 ceph34 ceph35 ceph36 ceph38 ceph39 ceph40 ceph41 ceph42"
+readonly ALL_NODES="ceph01 ceph02 ceph03"
+#ALL_NODES1="ceph01 ceph02 ceph03 ceph04 ceph05 ceph06 ceph07 ceph08 ceph09 ceph10"
+#ALL_NODES1="$ALL_NODES1 ceph11 ceph12 ceph13 ceph14 ceph15 ceph16 ceph17 ceph18 ceph19"
+#ALL_NODES1="$ALL_NODES1 ceph20 ceph21 ceph22 ceph23 ceph24 ceph25 ceph26 ceph27 ceph28 ceph29"
+#readonly ALL_NODES="$ALL_NODES1 ceph30 ceph31 ceph32 ceph33 ceph34 ceph35 ceph36 ceph38 ceph39 ceph40 ceph41 ceph42"
 
 # ALMOST CONSTANT
 
-BIN=ceph_ho_dumper.py
-TARGET="/tmp/$BIN"
-LOG=/tmp/ceph_ho_dumper.log
-RESULT=/tmp/historic_ops_dump.bin
-SRV_FILE=mira-ceph-ho-dumper.service
+readonly BIN=ceph_ho_dumper.py
+readonly TARGET="/tmp/${BIN}"
+readonly LOG=/tmp/ceph_ho_dumper.log
+readonly RESULT=/tmp/historic_ops_dump.bin
+readonly SRV_FILE=mira-ceph-ho-dumper.service
 
 # CONSTANTS
 
-DEFAULT_COUNT=20
-DEFAULT_DURATION=600
-SSH_CMD="ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-SCP_CMD="scp -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-SERVICE="$SRV_FILE"
-SRV_FILE_DST_PATH="/lib/systemd/system/$SRV_FILE"
+readonly RED='\033[0;31m'
+readonly GREEN='\033[0;32m'
+readonly NC='\033[0m' # No Color
 
+readonly DEFAULT_COUNT=20
+readonly DEFAULT_DURATION=600
+readonly SSH_CMD="ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+readonly SCP_CMD="scp -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+readonly SERVICE="${SRV_FILE}"
+readonly SRV_FILE_DST_PATH="/lib/systemd/system/${SRV_FILE}"
+readonly DEFAULT_JOBS=$(echo "${ALL_NODES}" | wc --words)
+
+
+function do_ssh {
+    local node="${1}"
+    local cmd="${2}"
+    set -x
+    echo "${cmd}" | ${SSH_CMD} "${node}" -- sudo bash
+    { set +x ; } 2>/dev/null
+}
+
+function do_ssh_out {
+    local node="${1}"
+    local cmd="${2}"
+    echo "${cmd}" | ${SSH_CMD} "${node}" -- sudo bash
+}
+
+function do_scp {
+    local source="${1}"
+    local target="${2}"
+    set -x
+    ${SCP_CMD} "${source}" "${target}"
+    { set +x ; } 2>/dev/null
+}
 
 function clean {
-    NODES="$1"
-    for NODE in $NODES ; do
-        CMD="systemctl stop $SERVICE ; systemctl disable $SERVICE"
-        CMD="$CMD ; $TARGET set --duration=$DEFAULT_DURATION --count=$DEFAULT_COUNT >/dev/null 2>&1 "
-        CMD="$CMD ; rm -f $SRV_FILE_DST_PATH $TARGET $LOG $RESULT"
-        set -x
-        echo "$CMD" | $SSH_CMD "$NODE" sudo bash || true
-        { set +x ; } 2>/dev/null
+    local nodes="${1}"
+    local cmd
+    for node in ${nodes} ; do
+        cmd="systemctl stop ${SERVICE} ; "
+        cmd+="systemctl disable ${SERVICE} ; "
+        cmd+="${TARGET} set --duration=${DEFAULT_DURATION} --count=${DEFAULT_COUNT} >/dev/null 2>&1 ; "
+        cmd+="rm -f ${SRV_FILE_DST_PATH} ${TARGET} ${LOG} ${RESULT} || true"
+        do_ssh "${node}" "${cmd}"
     done
 }
 
 function update_bin {
-    NODES="$1"
-    for NODE in $NODES ; do
-        CMD="chown root.root $TARGET && chmod +x $TARGET && sudo systemctl start $SERVICE"
-        set -x
-        $SSH_CMD "$NODE" sudo systemctl stop "$SERVICE"
-        $SCP_CMD "$BIN" "$NODE:$TARGET"
-        echo "$CMD" | $SSH_CMD "$NODE" sudo bash
-        { set +x; } 2>/dev/null
+    local nodes="${1}"
+    for node in ${nodes} ; do
+        do_ssh "${node}" "sudo systemctl stop ${SERVICE}"
+        do_scp "${BIN}" "${node}:${TARGET}"
+        do_ssh "${node}" "chown root.root ${TARGET} && chmod +x ${TARGET} && sudo systemctl start ${SERVICE}"
     done
 }
 
 function deploy {
-    NODES="$1"
-    for NODE in $NODES ; do
-        CMD="chown root.root $TARGET && chmod +x $TARGET"
-        CMD="$CMD && mv /tmp/$SRV_FILE $SRV_FILE_DST_PATH && chown root.root $SRV_FILE_DST_PATH"
-        CMD="$CMD && systemctl daemon-reload && systemctl enable $SERVICE && systemctl start $SERVICE"
+    local nodes="${1}"
+    local cmd
+    local srv_file
+    for node in ${nodes} ; do
+        do_scp "${BIN}" "${node}:${TARGET}"
 
-        set -x
-        $SCP_CMD "$BIN" "$NODE:$TARGET"
-        cat "$SRV_FILE" | sed --expression "s/{DURATION}/$RECORD_DURATION/" \
-                              --expression "s/{SIZE}/$RECORD_SIZE/" \
-                              --expression "s/{LOG_FILE}/${LOG//\//\\/}/" \
-                              --expression "s/{RESULT}/${RESULT//\//\\/}/" \
-                        | $SSH_CMD "$NODE" "cat > /tmp/$SRV_FILE"
-        echo "$CMD" | $SSH_CMD "$NODE" sudo bash
-        { set +x; } 2>/dev/null
+        srv_file=$(sed --expression "s/{DURATION}/${RECORD_DURATION}/" \
+                       --expression "s/{SIZE}/${RECORD_SIZE}/" \
+                       --expression "s/{LOG_FILE}/${LOG//\//\\/}/" \
+                       --expression "s/{RESULT}/${RESULT//\//\\/}/" < "${SRV_FILE}")
+
+        echo "${srv_file}" | ${SSH_CMD} "${node}" "cat > /tmp/${SRV_FILE}"
+
+        cmd="chown root.root ${TARGET} && "
+        cmd+="chmod +x ${TARGET} && "
+        cmd+="mv /tmp/${SRV_FILE} ${SRV_FILE_DST_PATH} &&"
+        cmd+="chown root.root ${SRV_FILE_DST_PATH} && "
+        cmd+="systemctl daemon-reload && "
+        cmd+="systemctl enable ${SERVICE} && "
+        cmd+="systemctl start ${SERVICE}"
+
+        do_ssh "${node}" "${cmd}"
     done
 }
 
 function show {
-    NODES="$1"
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    NC='\033[0m' # No Color
+    local nodes="${1}"
+    local srv_stat
+    local log_file_ll
+    local log_size
 
-    for NODE in $NODES ; do
-        SRV_STAT=$($SSH_CMD $NODE sudo systemctl status $SERVICE | grep Active || true)
-        LOG_FILE_LL=$($SSH_CMD $NODE ls -l $RESULT 2>&1 || true)
+    for node in ${nodes} ; do
 
-        if [[ "$LOG_FILE_LL" == *"No such file or directory"* ]] ; then
-            LOG_SIZE="NO FILE"
+        srv_stat=$(do_ssh_out "${node}" "sudo systemctl status ${SERVICE}" | grep Active || true)
+        log_file_ll=$(do_ssh_out "${node}" "ls -l ${RESULT}" 2>&1 || true)
+
+        if [[ "${log_file_ll}" == *"No such file or directory"* ]] ; then
+            log_size="NO FILE"
         else
-            LOG_SIZE=$(echo "$LOG_FILE_LL" | awk '{print $5}' | numfmt --to=iec-i --suffix=B)
+            log_size=$(echo "${log_file_ll}" | awk '{print $5}' | numfmt --to=iec-i --suffix=B)
         fi
 
-        if [[ "$SRV_STAT" == *" inactive "* ]] ; then
-            printf "%-20s : %b %s %b data_sz = %s\n" "$NODE" "$RED" "$SRV_STAT" "$NC" "$LOG_SIZE"
+        if [[ "${srv_stat}" == *" inactive "* ]] ; then
+            printf "%-20s : %b %s %b data_sz = %s\n" "${node}" "${RED}" "${srv_stat}" "${NC}" "${log_size}"
         else
-            if [[ "$SRV_STAT" == *" failed "* ]] ; then
-                printf "%-20s : %b %s %b data_sz = %s\n" "$NODE" "$RED" "$SRV_STAT" "$NC" "$LOG_SIZE"
+            if [[ "${srv_stat}" == *" failed "* ]] ; then
+                printf "%-20s : %b %s %b data_sz = %s\n" "${node}" "${RED}" "${srv_stat}" "${NC}" "${log_size}"
             else
-                printf "%-20s : %b %s %b data_sz = %s\n" "$NODE" "$GREEN" "$SRV_STAT" "$NC" "$LOG_SIZE"
+                printf "%-20s : %b %s %b data_sz = %s\n" "${node}" "${GREEN}" "${srv_stat}" "${NC}" "${log_size}"
             fi
         fi
     done
 }
 
 function stop {
-    NODES="$1"
-    for NODE in $NODES ; do
-        set -x
-        $SSH_CMD "$NODE" sudo systemctl stop "$SERVICE"
-        { set +x; } 2>/dev/null
+    local nodes="${1}"
+    for node in ${nodes} ; do
+        do_ssh "${node}" "sudo systemctl stop ${SERVICE}"
     done
 }
 
 function tails {
-    NODES="$1"
-    TSIZE="$2"
-    for NODE in $NODES ; do
-        echo $NODE
-        $SSH_CMD "$NODE" tail --lines "$TSIZE" "$LOG"
+    local nodes="${1}"
+    local tsize="${2}"
+    for node in ${nodes} ; do
+        echo "${node}"
+        $SSH_CMD "${node}" -- "tail --lines ${tsize} \"${LOG}\""
         echo
     done
 }
 
 function start {
-    NODES="$1"
-    for NODE in $NODES ; do
-        set -x
-        $SSH_CMD "$NODE" sudo systemctl start "$SERVICE"
-        { set +x; } 2>/dev/null
+    local nodes="${1}"
+    for node in ${nodes} ; do
+        do_ssh "${node}" "sudo systemctl start ${SERVICE}"
     done
 }
 
 function collect_one {
-    NODE="$1"
-    RBN=$(basename ${RESULT})
-    set -x
-    $SSH_CMD "$NODE" sudo "$COMPRESSOR" "$RESULT"
-    $SCP_CMD "$NODE:${RESULT}.${EXT}" "${NODE}-${RBN}.${EXT}"
-    $SSH_CMD "$NODE" sudo rm -f "${RESULT}.${EXT}"
-    { set +x; } 2>/dev/null
+    local node="${1}"
+    local rbn
+
+    rbn="$(basename "${RESULT}")"
+    do_ssh "${node}" "sudo ${COMPRESSOR} ${RESULT}"
+    do_scp "${node}:${RESULT}.${EXT}" "${node}-${rbn}.${EXT}"
+    do_ssh "${node}" "sudo rm --force ${RESULT}.${EXT}"
 }
 
 function collect {
-    for NODE in $1 ; do
-        collect_one "$NODE"
+    local nodes="${1}"
+    local target_dir="${2}"
+
+    pushd "${target_dir}" >/dev/null
+    for node in ${nodes} ; do
+        collect_one "${node}"
     done
+    popd  >/dev/null
 }
 
 function collect_parallel {
-    JOBS="$1"
-    NODES="$2"
-    NODES_COUNT=$(echo "$NODES" | wc --words)
+    local nodes="${1}"
+    local jobs="${2}"
+    local tgt_node="${3}"
+    local nodes_count
+    nodes_count="$(echo "${nodes}" | wc --words)"
 
-    MAX_NODE_PER_JOB=$((NODES_COUNT / JOBS))
-    if (( MAX_NODE_PER_JOB * JOBS != NODES_COUNT )); then
-        MAX_NODE_PER_JOB=$((MAX_NODE_PER_JOB + 1))
+    local max_nodes_per_job=$((nodes_count / jobs))
+    if (( max_nodes_per_job * jobs != nodes_count )); then
+        max_nodes_per_job=$((max_nodes_per_job + 1))
     fi
 
-    CNODES=""
-    COUNT=0
-    for NODE in $NODES ; do
-        CNODES="$CNODES $NODE"
-        COUNT=$((COUNT + 1))
-        if (( COUNT == MAX_NODE_PER_JOB )) ; then
-            collect "$CNODES" &
-            CNODES=""
-            COUNT=0
+    local curr_nodes=""
+    local count=0
+    for node in ${nodes} ; do
+        curr_nodes="${curr_nodes} ${node}"
+        count=$((count + 1))
+        if (( count == max_nodes_per_job )) ; then
+            collect "${curr_nodes}" "${tgt_node}" &
+            curr_nodes=""
+            count=0
         fi
     done
 
-    if [[ "$CNODES" != "" ]] ; then
-        collect "$CNODES" &
+    if [[ "${curr_nodes}" != "" ]] ; then
+        collect "${curr_nodes}" "${tgt_node}" &
     fi
 
     wait
 }
 
-while getopts "rcdsut:STlL" opt; do
-    case "$opt" in
-    c)  clean "$ALL_NODES"
+function main {
+    # parse/validate/prepare parameters for -l -L options
+    local tgt
+    local jobs
+
+    if [[ "${1}" == "-L" ]] ; then
+        case "$#" in
+        1) tgt="."
+           jobs="${DEFAULT_JOBS}"
+           ;;
+        2) tgt="${2}"
+           jobs="${DEFAULT_JOBS}"
+           ;;
+        3) tgt="${2}"
+           jobs="${3}"
+           ;;
+        *) echo "Incorrect options provided" 1>&2 && exit 1
+           ;;
+        esac
+
+        if ! [[ "${jobs}" =~ ^[0-9]+$ ]] ; then
+            echo "Incorrect job count: '${jobs}'" 1>&2
+            exit 1
+        fi
+    fi
+
+    if [[ "${1}" == "-l" ]] ; then
+        case "$#" in
+        1) tgt="."
+           ;;
+        2) tgt="${2}"
+           ;;
+        *) echo "Incorrect options provided" 1>&2 && exit 1
+           ;;
+        esac
+    fi
+
+    if [[ ("${1}" == "-l" || "${1}" == "-L") && "${tgt}" != "." && ! -d "${tgt}" ]] ; then
+        mkdir --parents "${tgt}"
+    fi
+
+    # main
+
+    case "${1}" in
+    -c) clean "${ALL_NODES}"
         ;;
-    u)  update_bin "$ALL_NODES"
+    -u) update_bin "${ALL_NODES}"
         ;;
-    l)  collect "$ALL_NODES"
+    -l) collect "${ALL_NODES}" "${tgt}"
         ;;
-#    L)  collect_parallel $(echo $ALL_NODES | wc -w) "$ALL_NODES"
-    L)  collect_parallel 2 "$ALL_NODES"
+    -L) collect_parallel "${ALL_NODES}" "${jobs}" "${tgt}"
         ;;
-    r)  clean "$ALL_NODES"
-        deploy "$ALL_NODES"
-        show "$ALL_NODES"
+    -r) clean "${ALL_NODES}"
+        deploy "${ALL_NODES}"
+        show "${ALL_NODES}"
         ;;
-    d)  deploy "$ALL_NODES"
+    -d) deploy "${ALL_NODES}"
         ;;
-    s)  show "$ALL_NODES"
+    -s) show "${ALL_NODES}"
         ;;
-    t)  tails "$ALL_NODES" "$OPTARG"
+    -t) local tsize
+        if [[ "$#" != 2 ]] ; then
+            tsize=10
+        else
+            tsize="${2}"
+        fi
+
+        if ! [[ "${tsize}" =~ ^[0-9]+$ ]] ; then
+            echo "Incorrect tail size: '${tsize}'" 1>&2
+            exit 1
+        fi
+
+        tails "${ALL_NODES}" "${tsize}"
+        shift
         ;;
-    S)  start "$ALL_NODES"
+    -S) start "${ALL_NODES}"
         ;;
-    T)  stop "$ALL_NODES"
+    -T) stop "${ALL_NODES}"
+        ;;
+    *)  echo "Incorrect options provided" 1>&2 && exit 1
         ;;
     esac
-done
+}
+
+main "$@"
